c22a1c29bc16be8eab58b9aa6363f1a590c7c72e
John Orndorff <johnornd@gmail.com>
2013-06-23 12:20:11 -0700

    Added a random cat link

--- hashacat.py
+++ hashacat.py
@@ -3,6 +3,8 @@
 from flask import Flask, jsonify, send_file, request, render_template
 import re
 import os, os.path
+import hashlib
+import random
 app = Flask(__name__)
 
 def updateCatCount():
@@ -14,7 +16,7 @@ def updateCatCount():
 def verifyHash(hash):
 		return bool(re.search("^([a-f0-9]{40})$|^([a-f0-9]{32})$", hash))
 
-@app.route('/hash/<hash>')
+@app.route('/cat/<hash>')
 def returnCat(hash):
 	if verifyHash(hash):
 		catID = int(hash, 16) % catCount
@@ -26,16 +28,23 @@ def returnCat(hash):
 			return send_file(catPath)
 	else:
 		return "Bad hash!"
-	
-
-@app.route('/')
-def displayIndex():
-	return render_template('mainPage.html')
 
 
 @app.route('/info')
 def displayInfo():
 	return render_template('info.html')
+	
+	
+@app.route('/randomHash')
+def getRandomHash():
+	hash = hashlib.sha1(str(random.random())).hexdigest()
+	return jsonify(hash=hash)
+
+
+@app.route('/')
+def displayIndex():
+	return render_template('mainPage.html')
+	
 
 if __name__ == '__main__':
 	app.run()
